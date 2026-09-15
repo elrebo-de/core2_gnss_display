84815d2cf1ac695ff3ac1e7a6e3fe60da768b4c1
Christoph.Oberle.ext <christoph.oberle@elrebo.de>
2026-09-15 18:27:00 +0200

    Version 1.1.12: Bugfix for Cockpit tabview: re-enable updates

--- main/main.cpp
+++ main/main.cpp
@@ -325,14 +325,14 @@ extern "C" void ppsSignalCb(void *arg, void *data)
             }
         }
 
+        // set current cockpit values
+        lv_gnss_cockpit_set_current_values(angle, speed, altitude, latitudeDegMinSec.c_str(), latitudeDeg.c_str(), longitudeDegMinSec.c_str(), longitudeDeg.c_str(), xdate.c_str(), xtime.c_str(), nrOfSats);
+
         // 2. Das automatische Vormerken von Änderungen stoppen (Freeze)
         bsp_display_lock(-1);
         lv_display_t *disp = lv_display_get_default();
         lv_display_enable_invalidation(disp, false);
         bsp_display_unlock();
-
-        // set current cockpit values
-        lv_gnss_cockpit_set_current_values(angle, speed, altitude, latitudeDegMinSec.c_str(), latitudeDeg.c_str(), longitudeDegMinSec.c_str(), longitudeDeg.c_str(), xdate.c_str(), xtime.c_str(), nrOfSats);
 
         // set marker position and speed in map
         if(sd_card) {
